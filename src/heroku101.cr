15d2267209bc4444a11a8b96898cba06a048aac1
Brian J. Cardiff <bcardiff@manas.com.ar>
2016-05-26 14:03:32 -0300

    allow --port PORT to change the port to use

--- src/heroku101.cr
+++ src/heroku101.cr
@@ -1,7 +1,14 @@
 require "http/server"
+require "option_parser"
 
 bind = "0.0.0.0"
 port = 8080
+
+OptionParser.parse! do |opts|
+  opts.on("-p PORT", "--port PORT", "define port to run server") do |opt|
+    port = opt.to_i
+  end
+end
 
 server = HTTP::Server.new(bind, port) do |context|
   context.response.content_type = "text/plain"
