ee514b7f722fe7592cc5b52db6271b9ad95f5916
Brian J. Cardiff <bcardiff@manas.com.ar>
2016-05-26 14:01:27 -0300

    local web server

--- src/heroku101.cr
+++ src/heroku101.cr
@@ -1,5 +1,12 @@
-require "./heroku101/*"
+require "http/server"
 
-module Heroku101
-  # TODO Put your code here
+bind = "0.0.0.0"
+port = 8080
+
+server = HTTP::Server.new(bind, port) do |context|
+  context.response.content_type = "text/plain"
+  context.response << "Hello world, got #{context.request.path}"
 end
+
+puts "Listening on http://#{bind}:#{port}"
+server.listen
